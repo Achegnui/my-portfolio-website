81e9068350171276081f6d94e6cc592994266d90
Achegnui Juven <achegnuijuven@gmail.com>
2024-08-17 11:43:07 +0100

    updated navbar with partial interactivity

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./NavLinks";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { FaTimes } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 
 const navlinks = [
   { path: "#about", title: "About" },
@@ -10,9 +15,10 @@ const navlinks = [
 ];
 // Navbar.jsx
 const NavBar = () => {
+  const [navbarOpen, setnavbarOpen] = useState(true);
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2 mt-4">
         <Link href="/">
           <Image
             src="/images/logo.jpg"
@@ -23,8 +29,25 @@ const NavBar = () => {
             className=""
           />
         </Link>
+        <div className="mobile-menu block md:hidden">
+          {navbarOpen ? (
+            <button
+              onClick={() => setnavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <IoMenu className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setnavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <FaTimes className="h-5 w-5" />
+            </button>
+          )}
+        </div>
 
-        <div className="menu block md:w-auto" id="navbar">
+        <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8">
             {navlinks.map((navlink, index) => (
               <li key={index}>
